7e849091e0cedca0d78a6d35f0247e926517b633
Ludovico Marino <113300950+F1nnLM@users.noreply.github.com>
2023-04-27 10:05:58 +0200

    Add files via upload

---
+++ defatriangle.py
@@ -0,0 +1,25 @@
+def main():
+    base = input("Triangle base lenght:")
+    side1 = input("First side lenght:")
+    side2 = input("Second side lenght:")
+    triangle = 0
+    if  side1 == side2:
+        if base != side1:
+            triangle = "isosceles"
+        else:
+            triangle = "equilateral"
+    elif side1 == base:
+        triangle = "isosceles"
+    elif side2 == base:
+        triangle = "isosceles"
+    else:
+        triangle = "scalene"
+    
+    print("the triangle is " + triangle)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
